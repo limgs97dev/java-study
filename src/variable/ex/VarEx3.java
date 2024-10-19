1838a24c798fa6aed2ef5d7d706466d63bfdde14
limgs97dev <limgs97dev@gmail.com>
2024-10-20 01:58:58 +0900

    add: variable

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class VarEx3 {
+
+  public static void main(String[] args) {
+    long num = 10_000_000_000L;
+    System.out.println(num);
+
+    boolean bool = true;
+    System.out.println(bool);
+  }
+}
