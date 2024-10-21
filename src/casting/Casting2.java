b91b0c70dc570478bbbc196e7d5397b7a50f7160
limgs97dev <limgs97dev@gmail.com>
2024-10-22 03:08:58 +0900

    added: scope, casting

---
+++ src/casting/Casting2.java
@@ -0,0 +1,15 @@
+package casting;
+
+public class Casting2 {
+
+  public static void main(String[] args) {
+    double doubleValue = 1.5;
+    int intValue = 0;
+
+    intValue = (int) doubleValue;
+    intValue = (int) 1.5;
+
+    System.out.println(intValue);
+  }
+
+}
