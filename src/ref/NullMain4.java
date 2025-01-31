1fae4510891999d4d0ef1ded3660e1a86a0b39e2
limgs <limgs97dev@gmail.com>
2025-02-01 07:48:26 +0900

    feat: ref 추가

---
+++ src/ref/NullMain4.java
@@ -0,0 +1,13 @@
+package ref;
+
+public class NullMain4 {
+    public static void main(String[] args) {
+        BigData bd = new BigData();
+        System.out.println("bigData.count => " + bd.count);
+        System.out.println("bigData.count => " + bd.data);
+
+        bd.data = new Data();
+        // NPE 안뜸
+        System.out.println("bigData.data.value => " + bd.data.value);
+    }
+}
